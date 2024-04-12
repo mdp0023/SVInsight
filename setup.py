f8d7666887f9e573dce9cc05a45dfe7b4883a773
mdp0023 <mattpreisser@gmail.com>
2024-04-12 09:25:52 -0500

    create setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='SVInsight',
+    version='',
+    license='MIT',
+    description='SVInsight - A python package for calculating an exploratory social vulnerability index',
+    author='M. Preisser, P. Passalacqua, R. P. Bixler',
+    author_email='mattpreisser@gmail.com',
+    
+)
