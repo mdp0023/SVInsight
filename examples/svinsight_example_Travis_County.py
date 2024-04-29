2de7baba341939ee78946a564b865c42ba52578a
mdp0023 <mattpreisser@gmail.com>
2024-04-29 10:21:53 -0500

    reformat naming convention

--- examples/svinsight_example_Travis_County.py
+++ examples/svinsight_example_Travis_County.py
@@ -7,83 +7,83 @@ sys.path.insert(0, '/Users/matthewpreisser/Documents/Research/Codes/SVInsight')
 from SVInsight import SVInsight as svi 
 import matplotlib.pyplot as plt
 
-# set variables
-project_name = 'Travis_County'
-file_path = "/Users/matthewpreisser/Documents/Research/Codes/SVInsight"
-api_key = os.environ.get('API_KEY')
-geoids = ['48453']
+# # set variables
+# project_name = 'Travis_County'
+# file_path = "/Users/matthewpreisser/Documents/Research/Codes/SVInsight"
+# api_key = os.environ.get('API_KEY')
+# geoids = ['48453']
 
-# create instance
-test = svi(project_name = project_name, 
-    file_path = file_path, 
-    api_key = api_key,
-    geoids=geoids)
+# # create instance
+# test = svi(project_name = project_name, 
+#     file_path = file_path, 
+#     api_key = api_key,
+#     geoids=geoids)
 
-# set the boundary and year variables
-boundary='bg'
-year=2017
-years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
-config_file='SVI'
+# # set the boundary and year variables
+# boundary='bg'
+# year=2017
+# years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+# config_file='SVI'
 
-# extract shapefile
-export = test.boundaries_data(boundary, year, overwrite=False)
+# # extract shapefile
+# export = test.boundaries_data(boundary, year, overwrite=False)
 
-# # extract raw census data
-test.census_data(boundary, 
-                    year, 
-                    interpolate=True,
-                    verbose=True,
-                    overwrite=False)
+# # # extract raw census data
+# test.census_data(boundary, 
+#                     year, 
+#                     interpolate=True,
+#                     verbose=True,
+#                     overwrite=False)
 
-# # # add variable
-# # test2.add_variable(boundary,
-# #                    year,
-# #                    'newvar',
-# #                    ['B03002_021E'])
+# # # # add variable
+# # # test2.add_variable(boundary,
+# # #                    year,
+# # #                    'newvar',
+# # #                    ['B03002_021E'])
 
-# print(test.all_vars_eqs['MEDAGE'])
-# test.var_descriptions(['MEDAGE','PPUNIT'])
+# # print(test.all_vars_eqs['MEDAGE'])
+# # test.var_descriptions(['MEDAGE','PPUNIT'])
 
-# # configure run and calculate svi
-test.configure_variables(config_file)
-test.calculate_svi(config_file, boundary, 2017)
-
-
-# # MULTI YEARS
-# for year in years:
-#     # extract shapefile
-#     export = test.boundaries_data(boundary, year, overwrite=False)
-
-#     # # extract raw census data
-#     test.census_data(boundary, year, interpolate=True,verbose=True,overwrite=False)
-
-#     # # configure run and calculate svi
-#     config_file='SVI'
-#     test.configure_variables(config_file)
-#     test.calculate_svi(config_file, boundary, year)
-#     geopackages = [year, boundary, config_file, 'FA_SVI_Percentile']
-#     figure = test.plot_svi(plot_option=1, geopackages=geopackages)
-#     figure.savefig(f"{os.getcwd()}/Travis_County/Figures/{year}_{boundary}_{config_file}_FA_SVI_Percentile.png", dpi=300)
+# # # configure run and calculate svi
+# test.configure_variables(config_file)
+# test.calculate_svi(config_file, boundary, 2017)
 
 
-######## Plotting ########
-# # plot svi single
-# geopackages = [year, boundary, config_file, 'FA_SVI_Percentile']
+# # # MULTI YEARS
+# # for year in years:
+# #     # extract shapefile
+# #     export = test.boundaries_data(boundary, year, overwrite=False)
 
-# figure = test.plot_svi(plot_option=1,
-#                        geopackages=geopackages)
+# #     # # extract raw census data
+# #     test.census_data(boundary, year, interpolate=True,verbose=True,overwrite=False)
 
-#plot svi double
-geopackages = [[2017, boundary, config_file, 'FA_SVI_Percentile'], [2017, boundary, config_file, 'RM_SVI_Percentile']]
-figure = test.plot_svi(plot_option=2,
-                       geopackages=geopackages)
+# #     # # configure run and calculate svi
+# #     config_file='SVI'
+# #     test.configure_variables(config_file)
+# #     test.calculate_svi(config_file, boundary, year)
+# #     geopackages = [year, boundary, config_file, 'FA_SVI_Percentile']
+# #     figure = test.plot_svi(plot_option=1, geopackages=geopackages)
+# #     figure.savefig(f"{os.getcwd()}/Travis_County/Figures/{year}_{boundary}_{config_file}_FA_SVI_Percentile.png", dpi=300)
 
 
-# # plot svi complete
-# geopackages = [2018, boundary, config_file]
-# figure = test.plot_svi(plot_option=3,
+# ######## Plotting ########
+# # # plot svi single
+# # geopackages = [year, boundary, config_file, 'FA_SVI_Percentile']
+
+# # figure = test.plot_svi(plot_option=1,
+# #                        geopackages=geopackages)
+
+# #plot svi double
+# geopackages = [[2017, boundary, config_file, 'FA_SVI_Percentile'], [2017, boundary, config_file, 'RM_SVI_Percentile']]
+# figure = test.plot_svi(plot_option=2,
 #                        geopackages=geopackages)
 
 
+# # # plot svi complete
+# # geopackages = [2018, boundary, config_file]
+# # figure = test.plot_svi(plot_option=3,
+# #                        geopackages=geopackages)
 
-plt.show()
+
+
+# plt.show()
